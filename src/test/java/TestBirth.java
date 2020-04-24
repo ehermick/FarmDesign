19387172d2adec69e29292b986af3fee3638e2a0
ehermick <45372760+ehermick@users.noreply.github.com>
2020-04-24 15:45:49 -0500

    Fix testing issue

--- src/test/java/TestBirth.java
+++ src/test/java/TestBirth.java
@@ -43,7 +43,7 @@ public class TestBirth {
         cow1 = new Cow("Cow", 100, 3, -30, 2);
         
         //Too young, high enough birthcycle
-        sheep2 = new Sheep("Sheep", 100, 0, 5, 2);
+        sheep2 = new Sheep("Sheep", 100, -40, 5, 2);
         
         //Too young, and too low birthcycle
         chicken3 = new Chicken("Chicken", 100, 0, 0, 2);
